f2d6312e76a564dfdcc9f6822a3365ba1da790bc
Anna Tran <anhkaehne@gmail.com>
2018-09-18 15:00:55 +1000

    challenge 1 completed

--- functions.js
+++ functions.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
-    
+
     $('.cta a').click(function() {
         $('html,body').animate({
             scrollTop: $('#tours').offset().top
         }, 500);
         return false;
     });
-    
+
     $('#tours li').on('click', function() {
         $('#location').val($('img', this).attr('alt'));
     });
+
+    // the jQuery slim included in the challenge did not support Ajax functionality. I have changed this to jQuery min.
+    $.ajax({
+      url: 'https://9ss7bxey8k.execute-api.ap-southeast-2.amazonaws.com/default/dummy_service',
+      success: function(result) {
+        console.log(result.Data);
+        for (let key in result.Data) {
+          // img will select all nodes, 'eq' let me select individual node by number
+          $('img').eq(key).attr('src', result.Data[key].node.frontmatter.cover)
+        }
+      }
+    }); // end ajax call
 })
